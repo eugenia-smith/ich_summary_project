9eadad8ff6ac36d4484e441c48826a0adcb89cac
Eugenia Smith <ev-genius4@mail.ru>
2024-09-26 10:18:12 +0200

    markup of the main section completed

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,5 +1,19 @@
+import styles from "./styles.module.css";
+import mainBanner from "../../assets/images/main_banner_full.png";
+
 function Main() {
-  return <></>;
+  return (
+    <main className={styles.main_container}>
+      <picture className={styles.main_banner}>
+        <img src={mainBanner} alt="" />
+      </picture>
+      <section className={styles.catalogue_section}>
+        <h2 className={styles.section_heading}>Catalogue</h2>
+        <hr />
+        <ul className={styles.catalogue_list}></ul>
+      </section>
+    </main>
+  );
 }
 
 export default Main;
